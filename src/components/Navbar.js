486159da4c7cabe8f8ea32a330578ab1b2614949
marioBytes <mario.bytes92@gmail.com>
2023-01-11 21:23:27 -0700

    Give ability to show cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Container, Navbar, Modal } from 'react-bootstrap'
 
-const navbar = () => {
+const NavbarComponent = () => {
+  const [show, setShow] = useState(false)
+
   return (
-    <Navbar expand="sm">
-      <Navbar.Brand href='/'>Shopping Cart</Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className='justify-content-end'>
-        <Button>Cart 0 Items</Button>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <Navbar expand="sm">
+        <Navbar.Brand href='/'>Shopping Cart</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className='justify-content-end'>
+          <Button onClick={() => setShow(true)}>Cart 0 Items</Button>
+        </Navbar.Collapse>
+      </Navbar>
+      <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Shopping Cart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h1>This is the modal body</h1>
+        </Modal.Body>
+      </Modal>
+    </>
   )
 }
 
-export default navbar
+export default NavbarComponent
