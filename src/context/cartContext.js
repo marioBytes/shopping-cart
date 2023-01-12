0b521b0d72d66c1b55bf1cb85977003d50bbe05a
marioBytes <mario.bytes92@gmail.com>
2023-01-11 21:51:02 -0700

    Create context to handle global state

---
+++ src/context/cartContext.js
@@ -0,0 +1,94 @@
+import { createContext, useState } from 'react'
+import { products, getProductData } from '../productsStore'
+
+export const CartContext = createContext({
+  items: [],
+  getProductQuantity: () => { },
+  addItemToCart: () => { },
+  removeItemFromCart: () => { },
+  deleteFromCart: () => { },
+  getTotalCost: () => { },
+})
+
+function CartProvider({ children }) {
+  const [cartProducts, setCartProducts] = useState([])
+
+  // [ { id: 1, quantity: 2 } ]
+  function getProductQuantity(id) {
+    const quantity = cartProducts.find(product => product.id === id)?.quantity
+
+    if (!quantity) {
+      return 0;
+    }
+
+    return quantity
+  }
+
+  function addItemToCart(id) {
+    const quantity = getProductQuantity(id)
+
+    if (quantity === 0) {
+      setCartProducts([...cartProducts, { id: id, quantity: 1 }])
+    } else {
+      setCartProducts(
+        cartProducts.map(cartProduct => (
+          cartProduct.id === id
+            ? { ...cartProduct, quantity: cartProduct.quantity + 1 }
+            : cartProduct
+        ))
+      )
+    }
+  }
+
+  function removeItemFromCart(id) {
+    const quantity = getProductQuantity(id)
+
+    if (quantity === 1) {
+      deleteFromCart(id)
+    } else {
+      setCartProducts(
+        cartProducts.map(cartProduct => (
+          cartProduct.id === id
+            ? { ...cartProduct, quantity: cartProduct.quantity - 1 }
+            : cartProduct
+        ))
+      )
+    }
+  }
+
+  function deleteFromCart(id) {
+    setCartProducts(cartProducts => (
+      cartProducts.filter(currentProduct => (
+        currentProduct.id !== id
+      )))
+    )
+  }
+
+  function getTotalCost() {
+    let totalCost = 0
+
+    cartProducts.map(cartItem => {
+      const productData = getProductData(cartItem.id)
+      totalCost += (productData.price * cartItem.quantity)
+    })
+
+    return totalCost
+  }
+
+  const contextValue = {
+    items: cartProducts,
+    getProductQuantity,
+    addItemToCart,
+    removeItemFromCart,
+    deleteFromCart,
+    getTotalCost,
+  }
+
+  return (
+    <CartContext.Provider value={contextValue}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export default CartProvider
