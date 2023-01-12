173b1abf4998fca00de1e2ab63465f883b83a02d
marioBytes <mario.bytes92@gmail.com>
2023-01-11 22:17:25 -0700

    Show cart info in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Button, Container, Navbar, Modal } from 'react-bootstrap'
 
+import { CartContext } from '../context/cartContext'
+import CartProduct from './CartProduct'
+
 const NavbarComponent = () => {
+  const cart = useContext(CartContext)
   const [show, setShow] = useState(false)
+
+  const productCount = cart.items.reduce((sum, product) => sum + product.quantity, 0)
 
   return (
     <>
@@ -10,7 +16,7 @@ const NavbarComponent = () => {
         <Navbar.Brand href='/'>Shopping Cart</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className='justify-content-end'>
-          <Button onClick={() => setShow(true)}>Cart 0 Items</Button>
+          <Button onClick={() => setShow(true)}>Cart {productCount} Items</Button>
         </Navbar.Collapse>
       </Navbar>
       <Modal show={show} onHide={() => setShow(false)}>
@@ -18,7 +24,17 @@ const NavbarComponent = () => {
           <Modal.Title>Shopping Cart</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h1>This is the modal body</h1>
+          {productCount > 0
+            ? <>
+              <p>Items in your cart</p>
+              {cart.items.map(product => (
+                <CartProduct key={product.id} product={product} />
+              ))}
+              <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
+              <Button variant="success">Purchase</Button>
+            </>
+            : <h1>Your cart is empty</h1>
+          }
         </Modal.Body>
       </Modal>
     </>
